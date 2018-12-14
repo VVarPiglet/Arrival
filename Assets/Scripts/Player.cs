95ea69f0c618ff4a07dd89be732ae3cab6facd4d
VVarPiglet <elijahthetate@gmail.com>
2018-12-13 21:35:43 -0800

    Adding the real base project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 1.0f;
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Single axisRawHorizontal = Input.GetAxisRaw("Horizontal");
+        Single axisRawVertical = Input.GetAxisRaw("Vertical");
+
+		if(axisRawHorizontal > 0.5f || axisRawHorizontal < -0.5f)
+        {
+            transform.Translate(new Vector3(axisRawHorizontal * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+
+        if(axisRawVertical > 0.5f || axisRawVertical < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, axisRawVertical * moveSpeed * Time.deltaTime, 0f));
+        }
+
+	}
+}
---
+++ Assets/Scripts/WorldControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldControl : MonoBehaviour {
+    Vector3 currentClickTarget;
+    public GameObject Sprout;
+
+    // Use this for initialization
+    void Start () {
+        currentClickTarget = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 origin = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                          Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.zero, 0f);
+            if (hit)
+            {
+                var hitObjectTransform = hit.transform;
+                print(hit.transform.gameObject.tag);
+                print("cords: " + origin.ToString());
+
+                Instantiate(Sprout, new Vector3(origin.x, origin.y, 0), hitObjectTransform.rotation);
+            }
+        }
+    }
+}
